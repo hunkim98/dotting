10d6a16677dd95b9d14030943d678a19f6cde724
Kim Dong Hun <hunkim98@gmail.com>
2023-07-07 16:24:52 +0900

    Add test for sideway extension

--- src/test/interactionLayer/select.test.tsx
+++ src/test/interactionLayer/select.test.tsx
@@ -70,7 +70,7 @@ describe("Dotting Component", () => {
     });
   });
 
-  it("tests select area extension to top", () => {
+  it("tests select area extension to top & bottom", () => {
     const columnCount = editor.getColumnCount();
     const rowCount = editor.getRowCount();
     const gridSquareLength = editor.getGridSquareLength();
@@ -105,6 +105,108 @@ describe("Dotting Component", () => {
     expect(selectedArea?.startPixelIndex).toStrictEqual({
       rowIndex: -1,
       columnIndex: 0,
+    });
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousedown", {
+        offsetX: canvasElement.width / 2,
+        offsetY: canvasElement.height / 2 + (gridSquareLength * rowCount) / 2,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousemove", {
+        offsetX: canvasElement.width / 2,
+        offsetY:
+          canvasElement.height / 2 +
+          (gridSquareLength * rowCount) / 2 +
+          gridSquareLength,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mouseup", {
+        offsetX: canvasElement.width / 2,
+        offsetY:
+          canvasElement.height / 2 +
+          (gridSquareLength * rowCount) / 2 +
+          gridSquareLength,
+      }),
+    );
+    const selectedArea2 = editor.getSelectedArea();
+    expect(selectedArea2?.endPixelIndex).toStrictEqual({
+      rowIndex: rowCount,
+      columnIndex: columnCount - 1,
+    });
+  });
+
+  it("tests select area extension to left & right", () => {
+    const columnCount = editor.getColumnCount();
+    const rowCount = editor.getRowCount();
+    const gridSquareLength = editor.getGridSquareLength();
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousedown", {
+        offsetX: canvasElement.width / 2 - (gridSquareLength * columnCount) / 2,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousemove", {
+        offsetX:
+          canvasElement.width / 2 -
+          (gridSquareLength * columnCount) / 2 -
+          gridSquareLength,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mouseup", {
+        offsetX:
+          canvasElement.width / 2 -
+          (gridSquareLength * columnCount) / 2 -
+          gridSquareLength,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    const selectedArea = editor.getSelectedArea();
+    expect(selectedArea?.startPixelIndex).toStrictEqual({
+      rowIndex: 0,
+      columnIndex: -1,
+    });
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousedown", {
+        offsetX: canvasElement.width / 2 + (gridSquareLength * columnCount) / 2,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mousemove", {
+        offsetX:
+          canvasElement.width / 2 +
+          (gridSquareLength * columnCount) / 2 +
+          gridSquareLength,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    fireEvent(
+      canvasElement,
+      new FakeMouseEvent("mouseup", {
+        offsetX:
+          canvasElement.width / 2 +
+          (gridSquareLength * columnCount) / 2 +
+          gridSquareLength,
+        offsetY: canvasElement.height / 2,
+      }),
+    );
+    const selectedArea2 = editor.getSelectedArea();
+    expect(selectedArea2?.endPixelIndex).toStrictEqual({
+      rowIndex: rowCount - 1,
+      columnIndex: columnCount,
     });
   });
 });
