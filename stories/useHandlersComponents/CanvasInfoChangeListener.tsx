e31c08bb3d311065aae7d36a6eafb1c8a5ab13de
Hun Kim <hunkim98@gmail.com>
2023-10-09 15:06:20 +0900

    Add canvas info change handler in useHandlers hook example storybook

---
+++ stories/useHandlersComponents/CanvasInfoChangeListener.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import {
+  CanvasHoverPixelChangeHandler,
+  CanvasInfoChangeHandler,
+} from "../../src/components/Canvas/types";
+import Dotting, { DottingRef } from "../../src/components/Dotting";
+import useHandlers from "../../src/hooks/useHandlers";
+import { PanZoom } from "../../src/utils/types";
+
+const CanvasInfoChangeListener = () => {
+  const ref = useRef<DottingRef>(null);
+  const {
+    addCanvasInfoChangeEventListener,
+    removeCanvasInfoChangeEventListener,
+  } = useHandlers(ref);
+  const [canvasPanZoom, setCanvasPanZoom] = useState<PanZoom | null>(null);
+  const [gridSquareSize, setGridSquareSize] = useState<number>(0);
+  const [topLeftCornerOffset, setTopLeftCornerOffset] =
+    useState<{
+      x: number;
+      y: number;
+    } | null>(null);
+
+  useEffect(() => {
+    const handleHoverPixelChangeHandler: CanvasInfoChangeHandler = ({
+      panZoom,
+      topLeftCornerOffset,
+      topRightCornerOffset,
+      bottomLeftCornerOffset,
+      bottomRightCornerOffset,
+      gridSquareSize,
+    }) => {
+      setCanvasPanZoom(panZoom);
+      setGridSquareSize(gridSquareSize);
+      setTopLeftCornerOffset(topLeftCornerOffset);
+    };
+
+    addCanvasInfoChangeEventListener(handleHoverPixelChangeHandler);
+    return () => {
+      removeCanvasInfoChangeEventListener(handleHoverPixelChangeHandler);
+    };
+  }, []);
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: `'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+      }}
+    >
+      <Dotting ref={ref} width={"100%"} height={300} />
+      <div
+        style={{
+          marginTop: 25,
+          display: "flex",
+          flexDirection: "column",
+          gap: 10,
+          marginBottom: 50,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <strong>Current Scale</strong>
+          {canvasPanZoom && <div>{canvasPanZoom.scale}</div>}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <strong>Current Offset</strong>
+          {canvasPanZoom && (
+            <div>
+              x : {canvasPanZoom.offset.x}, y: {canvasPanZoom.offset.y}
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <strong>Top Left Corner Canvas Offset</strong>
+          {canvasPanZoom && (
+            <div>
+              x : {topLeftCornerOffset.x}, y: {topLeftCornerOffset.y}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasInfoChangeListener;
--- stories/useHandlersComponents/index.ts
+++ stories/useHandlersComponents/index.ts
@@ -2,10 +2,12 @@ import CanvasElementEvents from "./CanvasElementEvents";
 import DataChangeListener from "./DataChangeListener";
 import HoverPixelListener from "./HoverPixelListener";
 import StrokeListener from "./StrokeListener";
+import CanvasInfoChangeListener from "./CanvasInfoChangeListener";
 
 export {
   StrokeListener,
   HoverPixelListener,
   CanvasElementEvents,
   DataChangeListener,
+  CanvasInfoChangeListener,
 };
