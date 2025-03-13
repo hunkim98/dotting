c2dd5150ef8916715f6ebbed08eff26c8d1c36c6
Hun Kim <hunkim98@gmail.com>
2025-03-13 12:10:42 -0400

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import typescript from "rollup-plugin-typescript2";
 
-const packageJson = require("./package.json");
+import packageJson from "./package.json";
+
+// const packageJson = require("./package.json");
 
 export default {
   input: "src/index.ts",
